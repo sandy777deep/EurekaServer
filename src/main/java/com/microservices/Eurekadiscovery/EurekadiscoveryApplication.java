4eb25a28e689b99dd33ce12c4341650b29952571
sandy777deep <66700051+sandy777deep@users.noreply.github.com>
2022-08-22 19:49:32 -0500

    first commit

---
+++ src/main/java/com/microservices/Eurekadiscovery/EurekadiscoveryApplication.java
@@ -0,0 +1,19 @@
+package com.microservices.Eurekadiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekadiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekadiscoveryApplication.class, args);
+	}
+
+	
+	
+}
---
+++ src/test/java/com/microservices/Eurekadiscovery/EurekadiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.Eurekadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
